7fb9f16c425c880a5bc1c24351c99b6d8a4b6b9c
Justfemi <ajibade.olufemi@yahoo.com>
2024-07-09 23:27:50 +0100

    ft:testing checkout links

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,14 @@
 // import React from 'react'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   return (
     <>
       <Header />
       <p>Cart Page</p>
+      <Link to="/checkout">Go to checkout</Link>
       <Footer />
     </>
   )
