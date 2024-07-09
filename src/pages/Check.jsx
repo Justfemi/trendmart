de44aabfdded6e1fd84e966b24b1b1759a0eaf48
Justfemi <ajibade.olufemi@yahoo.com>
2024-07-09 22:16:57 +0100

    ch:completed checkout experience desktop

--- src/pages/Check.jsx
+++ src/pages/Check.jsx
@@ -1,18 +1,66 @@
-// import React from 'react'
+import { useState } from 'react'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import product1 from "../assets/product1.svg";
+import success from "../assets/success.svg";
+import checkmark from "../assets/checksuccess.svg";
+import Modal from "../components/Modal";
+import { useNavigate } from 'react-router-dom';
 
 const Check = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleViewOrder = () => {
+    setIsModalOpen(false);
+    navigate('/cart');
+  }
+
+  const handleShopping = () => {
+    setIsModalOpen(false);
+    navigate('/');
+  }
+
   return (
     <>
       <Header />
-      <section className="w-[80%] mx-auto border border-[#E0E0E0] rounded-tr-custom-10 rounded-tl-custom-10 my-5">
-        <div className="py-5 px-6 border-b border-[#E0E0E0]">
+      <section className="sm:w-[80%] w-[95%] mx-auto border border-[#E0E0E0] rounded-tr-custom-10 rounded-tl-custom-10 my-5">
+        <div className="py-5 sm:px-6 px-2 border border-b border-[#E0E0E0] flex item-center justify-between">
           <h4 className=" font-medium text-xl">Checkout</h4>
+
+          <h5 className="text-[#6a1b9a] font-bold text-sm">Step 3 of 3</h5>
         </div>
 
-        <div className="w-[70%] border border-[#6a1b9a] rounded-custom-3 p-6 flex items-center justify-between bg-[#FBF9FC] mx-auto mt-6 mb-3">
+        <div className='sm:hidden block'>
+          <p className="text-[#717171] uppercase font-normal text-base my-4 mx-6">Order Details</p>
+
+          <div className="flex felx-col">
+            <div className="flex items-center justify-between p-1">
+              <div className="w-[80] h-[80] border border-[#e0e0e0] rounded-custom-6">
+                <img src={product1} alt="first product" className="w-full h-full"/>
+              </div>
+
+              <div classNAME="FLEX flex-1">
+                <h3 className="text-[#1b1818] font-medium text-sm">Dior and Stone Island T-Shirt, Oversized Fit</h3>
+
+                <div>
+                  <h3 className="text-[#1b1818] font-medium text-sm">$250</h3>
+                  <p className="text-[#717171] font-normal text-sm">3</p>
+                </div>
+              </div>
+            </div>
+          </div>          
+        </div>
+
+        <div className="w-[70%] border border-[#6a1b9a] rounded-custom-3 p-6 items-center justify-between bg-[#FBF9FC] mx-auto mt-6 mb-3 hidden sm:flex">
           <p>Total</p>
 
           <div className="text-right">
@@ -25,7 +73,7 @@ const Check = () => {
           </div>
         </div>
 
-        <div className="w-[70%] mx-auto border border-[#E0E0E0] rounded-br-custom-10">
+        <div className="w-[70%] mx-auto border border-[#E0E0E0] rounded-br-custom-10 rounded-bl-custom-10 hidden sm:block">
           <p className="text-[#717171] uppercase font-normal text-base my-4 mx-6">Order Details</p>
 
           <div className="w-full py-3 px-6 bg-[#E0E0E0] border border-[#E0E0E0] flex items-center justify-between">
@@ -133,69 +181,59 @@ const Check = () => {
             <p className="text-[#1B1818] font-normal text-sm basis-[12%]">$750</p>
           </div>
 
-          <div className=" border border-[#e0e0e0] w-[95%] mx-auto mt-3 rounded-custom-10 flex items-center justify-center">
-            <div className="basis-1/2 p-4">
-              <h3>Shipping Address</h3>
+          <div className=" border border-[#e0e0e0] w-[98%] mx-auto mt-3 rounded-br-custom-10 rounded-bl-custom-10 flex items-center justify-center">
+            <div className="basis-1/2 text-[#717171] text-sm font-normal p-2">
+              <h3 className="text-[#1b1818] text-base font-medium">Shipping Address</h3>
               <p>Olakunle Oluremi</p>
               <p>No 4 madiana close, off Dar-Es-Salam street, wuse 2 Abuja, Nigeria</p>
-              <p>Email Address: <span> olakunle.o@gmail.com </span></p>
-              <p>Phone Number: <span> +234 815 3777 444 </span></p>
-              <p>Email: <span> olakunle.o@gmail.com </span></p>
+              <p className="text-[#1b1818]">Email Address: <span className="text-[#717171]"> olakunle.o@gmail.com </span></p>
+              <p className="text-[#1b1818]">Phone Number: <span className="text-[#717171]"> +234 815 3777 444 </span></p>
             </div>
 
-            <div className="basis-1/2 p-4 border-l border-[#e0e0e0]">
-              <h3>Order Notes</h3>
-              <p>
+            <div className="basis-1/2 border-l border-[#e0e0e0] p-2">
+              <h3 className="text-[#1b1818] text-base font-medium">Order Notes</h3>
+              <p className="text-[#717171] text-sm font-normal">
                 Please ensure that the blue dress is in size M and includes the matching belt as shown in the product image. 
                 I would appreciate if the items could be gift-wrapped with a birthday card included. If possible, deliver before 
                 July 10th as it&apos;s a gift for my friend&apos;s birthday. Thank you!
               </p>
             </div>
           </div>
+
+          <div className="flex py-6 mt-6 px-4 justify-end">
+            <button className="uppercase px-20 py-2.5 bg-[#6A1B9A] hover:bg-transparent hover:text-[#6a1b9a] 
+                mt-3 rounded-custom-50 text-white text-bold border border-[#6a1b9a]"
+                onClick={handleOpenModal}
+            >Place order</button>
+          </div>
         </div>
 
-        {/* <div className="w-[70%] mx-auto border-l border-r border-b border-[#E0E0E0] flex items-center justify-center">
-          <div className="basis-1/2 p-4">
-            <h3>Shipping Address</h3>
-            <p>Olakunle Oluremi</p>
-            <p>No 4 madiana close, off Dar-Es-Salam street, wuse 2 Abuja, Nigeria</p>
-            <p>Email Address: <span> olakunle.o@gmail.com </span></p>
-            <p>Phone Number: <span> +234 815 3777 444 </span></p>
-            <p>Email: <span> olakunle.o@gmail.com </span></p>
-          </div>
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          <div className="relative p-2">
+            <img src={success} alt="success animation" />
+            <img src={checkmark} alt="" className='absolute sm:top-1 sm:right-[130px] top-2 right-[110px]'/>
+            <div className="mt-[-80px] text-center">
+              <h3 className="text-[#1b18181] font-medium text-base">Order Placed Succesfully</h3>
+              <p className='text-[#717171] font-normal text-sm'>
+                Thank you for shopping with us! Your order is placed successfully and we are thrilled to have you as a customer
+              </p>
+              <div className='flex flex-col w-full'>
+                <button className="uppercase px-6 py-2.5 bg-[#6A1B9A] mt-3 rounded-custom-50 text-white text-bold"
+                  onClick={handleViewOrder}
+                >
+                  view order details
+                </button>
 
-          <div className="basis-1/2 p-4 border-l border-[#e0e0e0]">
-            <h3>Order Notes</h3>
-            <p>
-              Please ensure that the blue dress is in size M and includes the matching belt as shown in the product image. 
-              I would appreciate if the items could be gift-wrapped with a birthday card included. If possible, deliver before 
-              July 10th as it&apos;s a gift for my friend&apos;s birthday. Thank you!
-            </p>
+                <button className="uppercase px-6 py-2.5 bg-white border border-[#6A1B9A] mt-3 rounded-custom-50 text-[#6A1B9A] text-bold"
+                  onClick={handleShopping}
+                >continue shopping</button>
+              </div>
+            </div>
           </div>
-        </div> */}
+        </Modal>
       </section>
       <Footer />
     </>
   )
 }
-
 export default Check
-      {/* <table className="min-w-full bg-white border border-gray-200">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 border-b">Name</th>
-            <th className="py-2 px-4 border-b">Price</th>
-            <th className="py-2 px-4 border-b">Quantity</th>
-            <th className="py-2 px-4 border-b">Total</th>
-            <th className="py-2 px-4 border-b">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="py-2 px-4 border-b"></td>
-            <td className="py-2 px-4 border-b"></td>
-            <td className="py-2 px-4 border-b"></td>
-            <td className="py-2 px-4 border-b"></td>
-          </tr>
-        </tbody>
-      </table> */}
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import CountryDropdown from "../components/countryDropdown";
-import progress from "../assets/progress1.svg";
 import mastercard from "../assets/mastercard.svg";
 import visacard from "../assets/visacard.svg";
 import amazon from "../assets/amazon.svg";
@@ -27,19 +26,16 @@ const Checkout = () => {
   return (
     <>
       <Header />
-      <section className="w-[80%] mx-auto border border-[#E0E0E0] rounded-tr-custom-10 rounded-tl-custom-10 my-5">
-        <div className="py-5 px-6 border-b border-[#E0E0E0]">
-          <h4 className=" font-medium text-xl">Checkout</h4>
-        </div>
+      <section className="sm:w-[80%] w-[95%] mx-auto border border-[#E0E0E0] rounded-tr-custom-10 rounded-tl-custom-10 my-5">
+        {submitted ? (
+          <div>
+            <div className="py-5 sm:px-6 px-2 border border-b border-[#E0E0E0] flex item-center justify-between">
+              <h4 className=" font-medium text-xl">Checkout</h4>
 
-        <div className="h-10 border-b border-[#E0E0E0]">
-          {/* stepper here */}
-          <img src={progress} alt="progress bar"/>
-        </div>
+              <h5 className="text-[#6a1b9a] font-bold text-sm">Step 2 of 3</h5>
+            </div>
 
-        <div className="w-[90%] mx-auto lg:p-6 p-2 my-8">
-          {submitted ? (
-            <>
+            <div className="w-[90%] mx-auto lg:p-6 p-2 my-8">
               <div className="sm:flex border border-[#e0e0e0] items-center justify-around mb-6 rounded-custom-10 hidden">
                 <div className="flex flex-col m-auto border-r border-[#e0e0e0] p-9 w-full items-center cursor-pointer">
                   <div className="rounded-custom-6 border border-[#e0e0e0] p-2 mb-2">
@@ -179,80 +175,90 @@ const Checkout = () => {
                   >next</button>
                 </div>
               </form>
-            </>
-          ) : (
-            <form>
-              <div className="lg:w-1/2 w-full flex-col justify-start">
-                <p className="text-xs font-medium mb-2 text-[#1B1818]">Country/Region</p>
-                <CountryDropdown />
-              </div>
+            </div>
+          </ div>
+        ) : (
+          <div>
+            <div className="py-5 sm:px-6 px-2 border-b border-[#E0E0E0] flex item-center justify-between">
+              <h4 className=" font-medium text-xl">Checkout</h4>
 
-              <p className="text-[#717171] uppercase font-normal text-xs-custom mt-4 mb-2.3">contact information</p>
+              <h5 className="text-[#6a1b9a] font-bold text-sm">Step 1 of 3</h5>
+            </div>
 
-              <div className="w-full">
-                <p className="text-xs font-medium mb-2 text-[#1B1818]">Full Name</p>
-                <input type="text" placeholder="Enter full name" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
-              </div>
-
-              <div className="w-full flex items-center flex-col md:flex-row justify-between mt-2.5">
-                <div className="md:w-[48%] w-full mb-2">
-                  <p className="text-xs font-medium mb-2 text-[#1B1818]">Email Address</p>
-                  <input type="email" placeholder="Enter email address" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+            <div className="w-[95%] mx-auto lg:p-6 p-1 my-8">
+              <form>
+                <div className="lg:w-1/2 w-full flex-col justify-start">
+                  <p className="text-xs font-medium mb-2 text-[#1B1818]">Country/Region</p>
+                  <CountryDropdown />
                 </div>
 
-                <div className="md:w-[48%] w-full mb-2">
-                  <p className="text-xs font-medium mb-2 text-[#1B1818]">Phone Number</p>
-                  <input type="text" placeholder="Enter phone number" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
-                </div>
-              </div>
+                <p className="text-[#717171] uppercase font-normal text-xs-custom mt-4 mb-2.3">contact information</p>
 
-              <p className="text-[#717171] uppercase font-normal text-xs-custom mt-4 mb-2.3">address</p>
-
-              <div className="w-full flex items-center flex-col md:flex-row justify-between mt-2.5">
-                <div className="md:w-[48%] w-full mb-2">
-                  <p className="text-xs font-medium mb-2 text-[#1B1818]">Address</p>
-                  <input type="email" placeholder="Enter street details" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                <div className="w-full">
+                  <p className="text-xs font-medium mb-2 text-[#1B1818]">Full Name</p>
+                  <input type="text" placeholder="Enter full name" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
                 </div>
 
-                <div className="md:w-[48%] w-full mb-2">
-                  <p className="text-xs font-medium mb-2 text-[#1B1818]">State</p>
-                  <input type="text" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                <div className="w-full flex items-center flex-col md:flex-row justify-between mt-2.5">
+                  <div className="md:w-[48%] w-full mb-2">
+                    <p className="text-xs font-medium mb-2 text-[#1B1818]">Email Address</p>
+                    <input type="email" placeholder="Enter email address" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                  </div>
+
+                  <div className="md:w-[48%] w-full mb-2">
+                    <p className="text-xs font-medium mb-2 text-[#1B1818]">Phone Number</p>
+                    <input type="text" placeholder="Enter phone number" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                  </div>
                 </div>
-              </div>
 
-              <div className="w-full flex items-center flex-col md:flex-row justify-between mt-2.5">
-                <div className="md:w-[48%] w-full mb-2">
-                  <p className="text-xs font-medium mb-2 text-[#1B1818]">City</p>
-                  <input type="text" placeholder="" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                <p className="text-[#717171] uppercase font-normal text-xs-custom mt-4 mb-2.3">address</p>
+
+                <div className="w-full flex items-center flex-col md:flex-row justify-between mt-2.5">
+                  <div className="md:w-[48%] w-full mb-2">
+                    <p className="text-xs font-medium mb-2 text-[#1B1818]">Address</p>
+                    <input type="email" placeholder="Enter street details" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                  </div>
+
+                  <div className="md:w-[48%] w-full mb-2">
+                    <p className="text-xs font-medium mb-2 text-[#1B1818]">State</p>
+                    <input type="text" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                  </div>
                 </div>
 
-                <div className="md:w-[48%] w-full mb-2">
-                  <p className="text-xs font-medium mb-2 text-[#1B1818]">ZIP Code</p>
-                  <input type="text" placeholder="Enter zip code" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                <div className="w-full flex items-center flex-col md:flex-row justify-between mt-2.5">
+                  <div className="md:w-[48%] w-full mb-2">
+                    <p className="text-xs font-medium mb-2 text-[#1B1818]">City</p>
+                    <input type="text" placeholder="" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                  </div>
+
+                  <div className="md:w-[48%] w-full mb-2">
+                    <p className="text-xs font-medium mb-2 text-[#1B1818]">ZIP Code</p>
+                    <input type="text" placeholder="Enter zip code" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full input-outline-purple"/>
+                  </div>
                 </div>
-              </div>
 
-              <div className="flex items-center gap-3 mt-4 mb-6">
-                <input type="checkbox" name="" id="" />
-                <p className="text-[#717171] font-normal text-base">Set as default shipping address</p>
-              </div>
+                <div className="flex items-center gap-3 mt-4 mb-6">
+                  <input type="checkbox" name="" id="" />
+                  <p className="text-[#717171] font-normal text-base">Set as default shipping address</p>
+                </div>
 
-              <p className="text-[#717171] uppercase font-normal text-xs-custom mb-2.3">Additional Information</p>
+                <p className="text-[#717171] uppercase font-normal text-xs-custom mb-2.3">Additional Information</p>
 
-              <div className="w-full">
-                <p className="text-xs font-medium mb-2 text-[#1B1818]">Additinal Information <span className="text-[#717171]">(Optional)</span></p>
-                <textarea placeholder="e.g. special notes for delivery, additional phone number" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full resize-none input-outline-purple"/>
-              </div>
+                <div className="w-full">
+                  <p className="text-xs font-medium mb-2 text-[#1B1818]">Additinal Information <span className="text-[#717171]">(Optional)</span></p>
+                  <textarea placeholder="e.g. special notes for delivery, additional phone number" className="border border-[#e0e0e0] rounded-custom-10 p-2.5 w-full resize-none input-outline-purple"/>
+                </div>
 
-              <div className="flex py-6 mt-6 justify-end">
-                <button className="uppercase px-20 py-2.5 bg-[#6A1B9A] hover:bg-transparent hover:text-[#6a1b9a] 
-                    \mt-3 rounded-custom-50 text-white text-bold border border-[#6a1b9a]"
-                    onClick={handleSubmit}
-                >next</button>
-              </div>
-            </form>
-          )}
-        </div>
+                <div className="flex py-6 mt-6 justify-end">
+                  <button className="uppercase px-20 py-2.5 bg-[#6A1B9A] hover:bg-transparent hover:text-[#6a1b9a] 
+                      mt-3 rounded-custom-50 text-white text-bold border border-[#6a1b9a]"
+                      onClick={handleSubmit}
+                  >next</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
       </section>
       <Footer />
     </>
